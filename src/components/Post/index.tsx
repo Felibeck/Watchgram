b5c8fa5a5cdd4009febb6d517797768709c7f28b
FELIPE BECKFORD SISIS <49255481@ort>
2026-05-27 12:06:03 -0300

    Empece el componente Post

---
+++ src/components/Post/index.tsx
@@ -0,0 +1,15 @@
+import type { Post } from "../../types/post"
+
+const Post = ( {foto, cantLikes}:Post ) =>
+{
+    return (
+        <>
+            <img src={foto} alt="postFoto" />
+
+            
+            <p>{cantLikes}</p>
+        </>
+    )
+}
+
+export default Post
---
+++ src/types/post.ts
@@ -0,0 +1,4 @@
+export type Post = {
+    foto: string;
+    cantLikes: number;
+}
